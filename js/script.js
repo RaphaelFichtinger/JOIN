e5c11bd79366b33b13dafbfae3bd0b1151c0baab
Raphael Fichtinger <ruffy.dev@gmail.com>
2024-01-18 12:12:10 +0100

    changed JS folder structure, added loading.js file

--- js/script.js
+++ js/script.js
@@ -13,22 +13,6 @@ async function loadUsers(){
     }
 }
 
-async function setItem(key, value) {
-	const payload = {key, value, token:STORAGE_TOKEN}
-	return fetch(STORAGE_URL, {method: 'POST', body: JSON.stringify(payload)})
-	.then(resp => resp.json())
-}
-
-async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => {
-        // Verbesserter code
-        if (res.data) {
-            return res.data.value;
-        } throw `Could not find data with key "${key}".`;
-    });
-}
-
 async function signUp() {
 	let name = document.getElementById('input-name');
 	let password = document.getElementById('input-password');
@@ -72,32 +56,4 @@ function showSuccessButton() {
     successButton.style.opacity = '1';
 }
 
-
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-   z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-     elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-     file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-         }
-        }
-       xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-     }
-   }
-  }
 
